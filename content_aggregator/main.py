1938e7f275e3f98066a6e27c51526f999f2ebdbf
reika-fujimura <reika.k.fujimura@gmail.com>
2022-04-21 19:46:06 -0400

    streamlit app

--- content_aggregator/main.py
+++ content_aggregator/main.py
@@ -7,29 +7,47 @@ from aggregator import Aggregator
 
 
 sources = ['TheVerge', 'TechChurch', 'WIRED', 'BuzzFeed']
+source_dic = {
+    'TheVerge': 'https://www.theverge.com/tech',
+    'TechChurch': 'https://techcrunch.com/',
+    'WIRED': 'https://www.wired.com/',
+    'BuzzFeed': 'https://www.buzzfeed.com/tech'
+}
 
 # header
 st.title('Tech News Aggregator')
-st.write("""
-Contents in popular tech sites.
-""")
+st.image('images/image.jpg')    
 
 # aggregators
 col1, col2 = st.columns(2)
 
-
 for i in range(len(sources) // 2):
-    col1.header(sources[i])
+    col1.header('['+sources[i]+'](%s)' % source_dic[sources[i]])
     c = col1.container()
     agg = Aggregator(sources[i])
     agg.parse_contents()
-    for i in range(len(agg.titles)):
-        c.write(agg.titles[i] + ' ' + '([link](%s))' % agg.urls[i])
+    for j in range(len(agg.titles)):
+        c.write('**'+agg.titles[j] + '** ' + '([link](%s))' % agg.urls[j])
+        txt = agg.summarize_text(agg.urls[j],n_sentence = 1)
+        c.write(txt)
 
 for i in range(len(sources) // 2, len(sources)):
-    col2.header(sources[i])
+    col2.header('['+sources[i]+'](%s)' % source_dic[sources[i]])
     c = col2.container()
     agg = Aggregator(sources[i])
     agg.parse_contents()
     for i in range(len(agg.titles)):
-        c.write(agg.titles[i] + ' ' +'([link](%s))' % agg.urls[i])
+        c.write('**'+agg.titles[j] + '** ' + '([link](%s))' % agg.urls[j])
+        txt = agg.summarize_text(agg.urls[j],n_sentence = 1)
+        c.write(txt)
+
+# aggregate (pattern 2)
+# for i in range(len(sources)):
+#     st.header('['+sources[i]+'](%s)' % source_dic[sources[i]])
+#     c = st.container()
+#     agg = Aggregator(sources[i])
+#     agg.parse_contents()
+#     for j in range(len(agg.titles)):
+#         c.write(agg.titles[j] + ' ' + '([link](%s))' % agg.urls[j])
+#         txt = agg.summarize_text(agg.urls[j])
+#         c.write(txt)
